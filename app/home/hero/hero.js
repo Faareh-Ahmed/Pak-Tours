d68857313b5e2347b757564d4ccc9d439f59f265
Faareh-Ahmed <faarehahmed@gmail.com>
2024-03-03 22:39:30 +0500

    Updated the Videos for Home page

--- app/home/hero/hero.js
+++ app/home/hero/hero.js
@@ -3,7 +3,7 @@ export default function Hero() {
     return (
         <>
 
-        <video src='videos/backgroundvid.mp4' className="w-screen h-screen object-cover" autoPlay loop   preload="none" muted/>
+        <video src='videos/backgroundvid1.mp4' className="w-screen h-screen object-cover" autoPlay loop   preload="none" muted/>
         </>
     );
 }
