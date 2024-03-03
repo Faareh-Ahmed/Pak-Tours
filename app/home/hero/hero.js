d7af89efeb30943dbf866b2fff1151f4dc6c4582
Faareh-Ahmed <faarehahmed@gmail.com>
2024-03-03 22:02:10 +0500

    Background Video Setup in Home Page

---
+++ app/home/hero/hero.js
@@ -0,0 +1,9 @@
+
+export default function Hero() {
+    return (
+        <>
+
+        <video src='videos/backgroundvid.mp4' className="w-screen h-screen object-cover" autoPlay loop   preload="none" muted/>
+        </>
+    );
+}
