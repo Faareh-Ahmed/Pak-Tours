3fe857c24e76ff526cbfc35830ffcf8c3a78603e
Faareh-Ahmed <faarehahmed@gmail.com>
2024-03-09 21:09:46 +0500

    Responsive Navbar added Google Menu Icon

--- app/home/nav/navbar.js
+++ app/home/nav/navbar.js
@@ -1,18 +1,31 @@
+'use client'
+import Menu from "@/components/icons/menu";
+import { useState } from "react";
 
 
 export default function Navbar() {
+
+    const [open, setOpen] = useState(false);
     return (
         <>
-            <nav className="   text-white flex justify-between items-center px-4 py-2 absolute w-full ">
-                <img src="images/Logo2.jpeg" alt='Logo' className=" rounded-full object-cover h-20 w-20"/>
-                <div className=" flex  mx-auto gap-4 font-bold list-none text-2xl">
-                    <li className=" p-4">Home</li>
-                    <li className=" p-4">About</li>
-                    <li className=" p-4">Carousel</li>
-                    <li className=" p-4">Contact</li>
+            <div className="w-full absolute "> {/* Remove the Absolute to Make the Menu working*/}
+                <div className=" flex flex-col   md:items-center md:justify-center md:flex-row text-2xl ">
+                    <div className=" flex flex-row  font-bold  text-2xl ">
+                        <img src="images/Logo2.jpeg" alt='Logo' className=" rounded-full object-cover h-20 w-20" />
+                        <button onClick={() => setOpen(!open)} className={`md:hidden focus:outline-none focus:shadow-outline ml-auto p-4 `}>
+                            <Menu />
+                        </button>
+                    </div>
+                    <nav className={`  flex-col flex-grow pb-4 md:pb-0 ${open ? 'flex' : 'hidden'} md:flex justify-center items-center md:flex-row text-black list-none `}>
+                        <li className=" p-4">Home</li>
+                        <li className=" p-4">About</li>
+                        <li className=" p-4">Carousel</li>
+                        <li className=" p-4">Contact</li>
+                    </nav>
                 </div>
+            </div>
 
-            </nav>
+
         </>
     );
 }
---
+++ components/icons/menu.js
@@ -0,0 +1,8 @@
+export default function Menu() {
+    return (
+        <>
+            <svg xmlns="http://www.w3.org/2000/svg" height="44" viewBox="0 -960 960 960" width="44"><path d="M120-240v-80h720v80H120Zm0-200v-80h720v80H120Zm0-200v-80h720v80H120Z" /></svg>
+        </>
+    );
+
+}
